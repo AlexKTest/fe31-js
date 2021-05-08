65212536934c722753638652791e35f6cf88cb59
Alexander <alex.krechik@gmail.com>
2021-05-08 17:34:41 +0300

    Prettify runner

--- runner.js
+++ runner.js
@@ -1,45 +1,45 @@
-const { spawn } = require("child_process");
-const fs = require("fs");
-const file = process.argv[2];
-const filePath = file;
-const exist = fs.existsSync(filePath);
+const { spawn } = require('child_process')
+const fs = require('fs')
+const file = process.argv[2]
+const filePath = file
+const exist = fs.existsSync(filePath)
 
 if (!exist) {
-  console.error(`File "${file}" is not exist`);
-  process.exit();
+  console.error(`File "${file}" is not exist`)
+  process.exit()
 }
 
-console.clear();
+console.clear()
 
 const runFile = () => {
-  const node = spawn("node", [filePath]);
-  let firstInput = true;
+  const node = spawn('node', [filePath])
+  let firstInput = true
   const clear = () => {
     if (firstInput) {
-      console.clear();
-      firstInput = false;
+      console.clear()
+      firstInput = false
     }
-  };
-  node.stdout.on("data", (data) => {
-    clear();
-    console.log(data.toString().trim());
-  });
-  node.stderr.on("data", (data) => {
-    const dataText = data.toString().trim();
+  }
+  node.stdout.on('data', (data) => {
+    clear()
+    console.log(data.toString().trim())
+  })
+  node.stderr.on('data', (data) => {
+    const dataText = data.toString().trim()
     if (
       !dataText.includes('Debugger attached') &&
       !dataText.includes('Waiting for the debugger to disconnect')
     ) {
-      clear();
+      clear()
       console.log()
       console.log('ERROR:')
-      console.log(dataText);
+      console.log(dataText)
     }
-  });
-};
+  })
+}
 
-runFile();
+runFile()
 
 fs.watch(filePath, () => {
-  runFile();
-});
+  runFile()
+})
