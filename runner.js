88a6986480e4eb8221b9d70fab163d8144889d91
Alexander <alex.krechik@gmail.com>
2021-05-08 17:27:21 +0300

    Using of spawn to print text immediately after getting

--- runner.js
+++ runner.js
@@ -1,30 +1,45 @@
-const { exec } = require('child_process')
-const fs = require('fs')
-const file = process.argv[2]
-const filePath = file
-const exist = fs.existsSync(filePath)
+const { spawn } = require("child_process");
+const fs = require("fs");
+const file = process.argv[2];
+const filePath = file;
+const exist = fs.existsSync(filePath);
 
 if (!exist) {
-  console.error(`File "${file}" is not exist`)
-  process.exit()
-} 
-
-console.clear()
-
-const runFile = () => {
-  exec(`node ${filePath}`, (_err, stdout, stderr) => {
-    if (stdout || stderr) {
-      console.clear()
-      console.log(stdout)
-      console.log(stderr)
-    } else {
-      return
-    }
-  })
+  console.error(`File "${file}" is not exist`);
+  process.exit();
 }
 
-runFile()
+console.clear();
+
+const runFile = () => {
+  const node = spawn("node", [filePath]);
+  let firstInput = true;
+  const clear = () => {
+    if (firstInput) {
+      console.clear();
+      firstInput = false;
+    }
+  };
+  node.stdout.on("data", (data) => {
+    clear();
+    console.log(data.toString().trim());
+  });
+  node.stderr.on("data", (data) => {
+    const dataText = data.toString().trim();
+    if (
+      !dataText.includes('Debugger attached') &&
+      !dataText.includes('Waiting for the debugger to disconnect')
+    ) {
+      clear();
+      console.log()
+      console.log('ERROR:')
+      console.log(dataText);
+    }
+  });
+};
+
+runFile();
 
 fs.watch(filePath, () => {
-  runFile()
-})
+  runFile();
+});
